10ecad971d2aa0510698d0c93eb327b59a832a23
Raziel Lloyd Sevilla <raziellloydsevilla32948@gmail.com>
2025-10-16 21:06:51 +0800

    programmed orders page

---
+++ src/assets/data/ordersContent.js
@@ -0,0 +1,61 @@
+import product1 from "../images/Pages/HeroBanner1.jpg";
+import product2 from "../images/Pages/HeroBanner2.jpg";
+import product3 from "../images/Pages/HeroBanner3.jpg";
+import product4 from "../images/Pages/Pr11.jpg";
+
+const ordersContent = [
+  {
+    date: "October 15, 2024",
+    status: "Out for Delivery",
+    icon: "fa-truck",
+    product: "10M White Sport Boxing Adhesive Tape Strain Injury Support Binding Badge",
+    variation: "3.8cm*10m - For Wrist",
+    quantity: 1,
+    price: 58,
+    code: "FS7",
+    orderId: "BB01",
+    seller: "Fitness Shop",
+    image: product1,
+  },
+  {
+    date: "October 15, 2024",
+    status: "Processing",
+    icon: "fa-box",
+    product: "Hand Grip Strengthener Adjustable Resistance Forearm Exerciser",
+    variation: "Black 40KG",
+    quantity: 1,
+    price: 199,
+    code: "HS5",
+    orderId: "BB02",
+    seller: "Gym Essentials",
+    image: product2,
+  },
+  {
+    date: "October 31, 2024",
+    status: "Completed",
+    icon: "fa-check",
+    product: "Resistance Bands Set Workout Elastic Training Bands with Handles",
+    variation: "Set of 5",
+    quantity: 1,
+    price: 459,
+    code: "RB9",
+    orderId: "BB03",
+    seller: "FlexGear PH",
+    image: product3,
+  },
+  {
+    date: "October 31, 2024",
+    status: "Completed",
+    icon: "fa-check",
+    product: "Yoga Mat 10mm Thick Non-Slip Fitness Gym Exercise Pad",
+    variation: "Pink 10mm",
+    quantity: 1,
+    price: 350,
+    code: "YM1",
+    orderId: "BB04",
+    seller: "FitLife",
+    image: product4,
+  },
+];
+
+export default ordersContent;
