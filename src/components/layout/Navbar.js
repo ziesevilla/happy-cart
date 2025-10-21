13d63d5ad01f7d102dfc1efa7a522f67744a5467
Jericho Varde <vardejericho@gmail.com>
2025-10-21 20:51:20 +0800

    Make 'HAPPY CART' text redirect to homepage using Link

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,65 @@
 import React from "react";
 import "../../styles/component/Navbar.css";
 import logo from "../../assets/images/happy-cart.png";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom"; // ✅ include Link
 import { FaUser, FaHeart, FaShoppingCart, FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      {/* Left */}
+      {/* ===== Left Section ===== */}
       <div className="navbar-left">
-        <img src={logo} alt="Happy Cart Logo" className="navbar-logo" />
-        <h1 className="navbar-title">HAPPY CART</h1>
+        <Link to="/" className="navbar-home-link">   {/* ✅ Redirect to homepage */}
+          <img src={logo} alt="Happy Cart Logo" className="navbar-logo" />
+          <h1 className="navbar-title">HAPPY CART</h1>
+        </Link>
       </div>
 
-      {/* Middle Links */}
+      {/* ===== Middle Links ===== */}
       <ul className="navbar-links">
-        <li><NavLink to="/new" className={({ isActive }) => isActive ? "active-link" : ""}>New & Featured</NavLink></li>
-        <li><NavLink to="/men" className={({ isActive }) => isActive ? "active-link" : ""}>Men</NavLink></li>
-        <li><NavLink to="/women" className={({ isActive }) => isActive ? "active-link" : ""}>Women</NavLink></li>
-        <li><NavLink to="/kids" className={({ isActive }) => isActive ? "active-link" : ""}>Kids</NavLink></li>
-        <li><NavLink to="/sale" className={({ isActive }) => isActive ? "active-link" : ""}>Sale</NavLink></li>
+        <li>
+          <NavLink
+            to="/new"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            New & Featured
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/men"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            Men
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/women"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            Women
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/kids"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            Kids
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/sale"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            Sale
+          </NavLink>
+        </li>
       </ul>
 
-      {/* Right */}
+      {/* ===== Right Section ===== */}
       <div className="navbar-right">
         <div className="search-container">
           <input type="text" placeholder="Search" />
